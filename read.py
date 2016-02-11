375d86bef06383b01423088b6d21e34eb593b4fd
Mario Ynocente Castro <ycmario@gmail.com>
2016-02-11 11:03:14 +0100

    print ASS_ASSIGNMENT

--- read.py
+++ read.py
@@ -6,10 +6,13 @@ data = pandas.read_csv('train_2011_2012.csv', sep=';')
 #data.info()
 #print data.head()
 
-columns_to_bar = ['DAY_OFF', 'WEEK_END', 'TPER_HOUR', 'ACD_COD', 'CSPL_ABNCALLS1',
-            'CSPL_ABNCALLS2', 'CSPL_ABNCALLS3', 'CSPL_ABNCALLS4', 'CSPL_ABNCALLS5',
-            'CSPL_ABNCALLS6', 'CSPL_ABNCALLS7', 'CSPL_ABNCALLS8', 'CSPL_ABNCALLS9',
-            'CSPL_INCOMPLETE']
+print data['SPLIT_COD'].value_counts().size
+#print data['ACD_COD'].value_counts().size -> 1
+#print data['ASS_ASSIGNMENT'].value_counts().size
+S = set(data['ASS_ASSIGNMENT'].value_counts().axes[0])
+print S
+
+columns_to_bar = ['ASS_ASSIGNMENT']
 
 for column in columns_to_bar:
     data[column].value_counts(sort=False).plot.bar(title=column)
